c80a64400317e99ff06f0b7c82912c7baa81718c
Raizinhoo <gersinhowasd@gmail.com>
2024-03-15 15:15:30 -0300

    Criação editora completa

---
+++ core/serializers/editora.py
@@ -0,0 +1,8 @@
+from rest_framework.serializers import ModelSerializer
+
+from core.models import Editora
+
+class EditoraSerializer(ModelSerializer):
+    class Meta:
+        model=Editora
+        fields= ("id", "nome", "site")
