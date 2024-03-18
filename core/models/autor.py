cc0c5291c4923743cc4d5888f69805192f0f0e84
Raizinhoo <gersinhowasd@gmail.com>
2024-03-18 12:08:14 -0300

    valor padrão para nome autor para corrigir erro na migração

--- core/models/autor.py
+++ core/models/autor.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Autor(models.Model):
-    nome = models.CharField(max_length=100)
+    nome = models.CharField(max_length=100, default='Autor')
     
     def __str__(self) -> str:
         return self.nome
